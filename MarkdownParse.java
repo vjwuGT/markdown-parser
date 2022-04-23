038aca1f8653a5dabd2c6818ce56e287e609b5de
wuvic21 <victorwu689@gmail.com>
2022-04-22 18:49:27 -0700

    fixed test 6
    - fixed image links counting as links when they shouldnt
    - symptom was page.com showing as a link
    - bug was it did not ignore the image links

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,13 +16,13 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.lastIndexOf(")")) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            /* if (markdown.substring(openBracket - 1, openBracket).equals("!")){
+            if (markdown.substring(openBracket - 1, openBracket).equals("!")){
                 int closeBracket = markdown.indexOf("]", openBracket);
                 int openParen = markdown.indexOf("(", closeBracket);
                 int closeParen = markdown.indexOf(")", openParen);
                 currentIndex = closeParen + 1;
             }
-            else{ */
+            else{
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
@@ -30,7 +30,7 @@ public class MarkdownParse {
             currentIndex = closeParen + 1;
             }
       
-        //}
+        }
 
         return toReturn;
    }
